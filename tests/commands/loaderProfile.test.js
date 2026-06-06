04c85233df0804b27e304bcce56f3b1967876244
GoldCobra <toby.rapp90@gmail.com>
2026-06-06 18:48:29 +0200

    Assert report command game choices

--- tests/commands/loaderProfile.test.js
+++ tests/commands/loaderProfile.test.js
@@ -10,4 +10,22 @@ describe('futbot command loader profile', () => {
             'report2v2'
         ]));
     });
+
+    it('registers report commands with required game choices', () => {
+        const commands = commandLoader.loadCommandsForRegistration('futbot');
+        const report1v1 = commands.find(command => command.name === 'report1v1');
+        const report2v2 = commands.find(command => command.name === 'report2v2');
+
+        for (const command of [report1v1, report2v2]) {
+            const gameOption = command.options.find(option => option.name === 'game');
+            expect(gameOption).toEqual(expect.objectContaining({
+                required: true,
+                choices: [
+                    { name: 'MSBL', value: 'MSBL' },
+                    { name: 'MSC', value: 'MSC' },
+                    { name: 'SMS', value: 'SMS' }
+                ]
+            }));
+        }
+    });
 });
